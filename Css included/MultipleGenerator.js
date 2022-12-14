358709444a219cd517a9339b9b398cfcea635f53
Niraj018 <Niraj018>
2022-12-14 17:23:16 +0545

    Css and auto button switch

---
+++ Css included/MultipleGenerator.js
@@ -0,0 +1,55 @@
+function mulone(){
+    var multiply1 = document.getElementsByClassName("one-multiple");
+    for (i=1; i<=10; i++){
+        multiply1[i].textContent = 1 + "x" + i + "=" + 1*i;
+    }
+}
+function multwo(){
+    var multiply2 = document.getElementsByClassName("two-multiple");
+    for (i=1; i<=10; i++){
+        multiply2[i].textContent =2 + "x" + i + "=" + 2*i;
+    }
+}
+function multhree(){
+    var num = 3;
+    var multiply3 = document.getElementsByClassName("three-multiple");
+    for (i=1; i<=10; i++){
+        multiply3[i].textContent =3 + "x" + i + "=" + 3*i;
+    }
+}
+function mulfour(){
+    var multiply4 = document.getElementsByTagName("p");
+    for (i=1; i<=10; i++){
+        multiply4[i].textContent =4 + "x" + i + "=" + 4*i;
+    }
+}
+function mulfive(){
+    var multiply5 = document.getElementsByTagName("p");
+    for (i=1; i<=10; i++){
+        multiply5[i].textContent =5 + "x" + i + "=" + 5*i;
+    }
+}
+function mulsix(){
+    var multiply6 = document.getElementsByTagName("p");
+    for (i=1; i<=10; i++){
+        multiply6[i].textContent =6 + "x" + i + "=" + 6*i;
+    }
+}
+function mulseven(){
+    var multiply7 = document.getElementsByTagName("p");
+    for (i=1; i<=10; i++){
+        multiply7[i].textContent =7 + "x" + i + "=" + 7*i;
+    }
+}
+function muleight(){
+    var multiply8 = document.getElementsByTagName("p");
+    for (i=1; i<=10; i++){
+        multiply8[i].textContent =8 + "x" + i + "=" + 8*i;
+    }
+}
+function mulnine(){
+    var multiply9 = document.getElementsByTagName("p");
+    for (i=1; i<=10; i++){
+        multiply9[i].textContent =9 + " x " + i + " = " + 9*i;
+    }
+}
--- Selectors/tagName.js
+++ Selectors/tagName.js
@@ -1,6 +1,6 @@
 var tagname = document.getElementsByTagName('p');
 console.log(tagname)
 for(i=0;i<tagname.length;i++){
-    tagname[i].style.backgroundColor = "red";
-    tagname[i].style.color = "black";
+    tagname[i].style.backgroundColor = "#B98FE3";
+    tagname[i].style.color = "#3D3842";
 }
